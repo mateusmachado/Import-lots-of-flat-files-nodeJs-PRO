7d6b5c72dd3802bb2b6c59d1a98d1248ad587be9
Mateus <mmachado.csp@gmail.com>
2016-09-16 09:38:32 -0300

    adjusted file

--- transform/parseFile.js
+++ transform/parseFile.js
@@ -10,6 +10,15 @@ const lineID = 0;
 const dataID = 1;
 const itemID = 2;
 
+//
+let total =0;
+let bigSale = bigDecimal.ZERO;
+let smallSale = bigDecimal.ZERO;
+let idBestSales = 0;
+let worstSalesman = 3;
+let nameWorstSalesman;
+//
+
 split.splittedFile().forEach(function(value){
   parseValues(value); 
 });
@@ -28,36 +37,38 @@ function parseValues(value) {
       let price = split.priceValues(itemID, value, item);
 
 	    setTotal(quantity, price);
- 	    // setIdBestSale(value);   
-	    // setNameWorstSalesman(value);                                   
+
+
+ 	    setIdBestSale(value);  
+	    setNameWorstSalesman(value);                                   
  } 
 }
 
 function setTotal(quantity, price){
-  sale.total = bigDecimal.ZERO;	
+  total = bigDecimal.ZERO;	
   quantity.forEach(priceItem => {
     priceItem = new bigDecimal(quantity).multiply(new bigDecimal(price));
-    sale.total = sale.total.add(priceItem);    
+    total = total.add(priceItem);    
   });	
 }
 
 function setIdBestSale(value) {
-  if (sale.total.compareTo(sale.bigSale) == 1) {
-    sale.bigSale = sale.bigSale.add(sale.total);
-	  salesman.idBestSales = value[dataID];
+  if (total.compareTo(bigSale) == 1) {
+     bigSale = bigSale.add(total);
+	   idBestSales = value[dataID];
   }
 }
 
 function setNameWorstSalesman(value) {
-  if ((sale.total.compareTo(sale.smallSale) == -1) || (sale.smallSale.compareTo(bigDecimal.ZERO) == 0)) {
-    sale.smallSale = sale.smallSale.add(sale.total);
-    salesman.nameWorstSalesman = value[salesman.worstSalesman];
+  if ((total.compareTo(smallSale) == -1) || (smallSale.compareTo(bigDecimal.ZERO) == 0)) {
+    smallSale = smallSale.add(total);
+    nameWorstSalesman = value[worstSalesman];
   } 	
 }
 
 
 module.exports = {
 	outputValues: function() {
-	    return 'SalesMan: ' +salesman.amount+ '\nCustomers: ' +customer.amount+ '\nID of the most expensive sale: ' +salesman.idBestSales+ '\nWorst salesman ever: ' +salesman.nameWorstSalesman;          
+	    return 'SalesMan: ' +salesman.amount+ '\nCustomers: ' +customer.amount+ '\nID of the most expensive sale: ' +idBestSales+ '\nWorst salesman ever: ' +nameWorstSalesman;          
 	}		
 };
